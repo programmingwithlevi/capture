092bdb6cd087c4e230151270600e997b0dfcfdf6
Levi <95010008+programmingwithlevi@users.noreply.github.com>
2021-12-01 14:42:52 +0700

    2. About Us Section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+//import pages
+import AboutUs from '../src/pages/AboutUs';
 
 function App() {
   return (
     <div className="App">
-     <h1>Film</h1>
+     <AboutUs/>
     </div>
   );
 }
 
 export default App;
+
