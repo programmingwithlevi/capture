a1b2d278d136e04992f40a6352ff67901aef4d65
Levi <95010008+programmingwithlevi@users.noreply.github.com>
2021-12-03 12:47:12 +0700

    6. Styling Service Section

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -5,49 +5,79 @@ import diaphram from '../img/diaphram.svg';
 import money from '../img/money.svg';
 import teamwork from '../img/teamwork.svg';
 import home2 from '../img/home2.png';
+//styles
+import {About,Description,Image} from "../styles";
+import styled from "styled-components";
 
 const ServicesSection = () => {
     return(
-        <div className="services">
+        <About>
             <div className="description">
                 <h2>High <span>quality</span> services</h2>
-                <div className="cards">
-                    <div className="card">
+                <Cards>
+                    <Card>
                         <div className="icon">
                             <img alt="icon" src={clock} />
                             <h3>Efficient</h3>
                         </div>
                         <p>lorem5 </p>
-                    </div>
-                    <div className="card">
+                    </Card>
+                    <Card>
                         <div className="icon">
                             <img alt="icon" src={teamwork} />
                             <h3>Teamwork</h3>
                         </div>
                         <p>lorem5 </p>
-                    </div>
-                    <div className="card">
+                    </Card>
+                    <Card>
                         <div className="icon">
                             <img alt="icon" src={diaphram} />
                             <h3>Diaphram</h3>
                         </div>
                         <p>lorem5 </p>
-                    </div>
-                    <div className="card">
+                    </Card>
+                    <Card>
                         <div className="icon">
                             <img alt="icon" src={money} />
                             <h3>Affordable</h3>
                         </div>
                         <p>lorem5 </p>
-                    </div>
-                </div>
+                    </Card>
+                </Cards>
             </div>
         <div className="image">
             <img alt="camera" src={home2} />
 
         </div>
-        </div>
+        </About>
     )
 }
+
+const Services = styled(About)`
+h2{
+    padding-bottom: 5rem;
+}
+p{
+    width: 70%;
+    padding: 2rem 0rem 4rem 0rem;
+}
+`;
+const Cards = styled.div`
+display: flex;
+flex-wrap: wrap;
+`;
+const Card = styled.div`
+    flex-basis: 15rem;
+    .icon { 
+        display: flex;
+        align-items: center;
+        h3 {
+            margin-left: 1rem;
+            background: white;
+            color: black;
+            padding: 1rem;
+        }
+    }
+`;
 
 export default ServicesSection;
