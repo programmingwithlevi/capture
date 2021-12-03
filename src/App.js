3dbd6b238d11709615f746715a8e91f7fc67423d
Levi <95010008+programmingwithlevi@users.noreply.github.com>
2021-12-03 14:09:51 +0700

    Nav

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 //import pages
 import AboutUs from '../src/pages/AboutUs';
 import GlobalStyle from "./components/GlobalStyle";
+//import Nav from "../capture/src/components/Nav.js";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-     <AboutUs/>
+      
+      <AboutUs />
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Nav = () => {
+    return(
+        <StyledNav>
+            <h1><a href="#">Capture</a></h1>
+            <ul>
+                <li>
+                    <a href="#">1. About Us</a>
+                </li>
+                <li>
+                    <a href="#">2. Our Work</a>
+                </li>
+                <li>
+                    <a href="#">3. Contact Us</a>
+                </li>
+            </ul>
+        </StyledNav>
+    )
+}
+
+const StyledNav = styled.nav`
+    min-height: 10vh;
+    display: flex;
+    margin: auto;
+    justify-content: space-between;
+`;
+
+export default Nav;
