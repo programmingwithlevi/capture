4c43408a80bc6cc072452b0ba3d161667bb3b7af
Levi <95010008+programmingwithlevi@users.noreply.github.com>
2021-12-09 13:56:47 +0700

    11. Movie Details WORKING THIS TIME

--- src/movieState.js
+++ src/movieState.js
@@ -3,8 +3,8 @@ import athlete from './img/athlete-small.png';
 import goodtimes from './img/goodtimes-small.png';
 import theracer from './img/theracer-small.png';
 import athlete2 from './img/athlete2.png';
-import goodtimes2 from './img/good-times2.jpg';
-import theracer2 from './img/the-racer2.jpg';
+import goodtimes2 from './img/good-times2.jpeg';
+import theracer2 from './img/the-racer2.jpeg';
 
 export const MovieState = () => {
   return [
@@ -79,3 +79,4 @@ export const MovieState = () => {
     },
   ];
 };
+//export default movieState;
